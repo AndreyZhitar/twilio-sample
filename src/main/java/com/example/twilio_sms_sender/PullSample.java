4a6723dc4e4cb65a78231b2fb2d7b750d79558b9
AndreyZhitar <Andreyzhitar1106@gmail.com>
2021-05-23 20:58:09 +0300

    New branch

---
+++ src/main/java/com/example/twilio_sms_sender/PullSample.java
@@ -0,0 +1,13 @@
+package com.example.twilio_sms_sender;
+
+/**
+ * PullSample
+ *
+ * @author Andrey Zhitar
+ */
+public class PullSample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
