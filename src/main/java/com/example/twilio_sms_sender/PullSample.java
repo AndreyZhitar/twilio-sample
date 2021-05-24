53565117374ae23a1a485cfc92d9579d7bffc3fb
AndreyZhitar <Andreyzhitar1106@gmail.com>
2021-05-24 19:35:17 +0300

    some print

--- src/main/java/com/example/twilio_sms_sender/PullSample.java
+++ src/main/java/com/example/twilio_sms_sender/PullSample.java
@@ -10,5 +10,6 @@ public class PullSample {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Silly World!!!");
+        System.out.println("One more sout");
     }
 }
